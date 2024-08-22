223fdd1c8925d50d8aaccf975fc91c4119b4d55a
Liisa Davydov <liisamuttonen@gmail.com>
2024-08-22 13:21:38 +0300

    Add lecture notes

---
+++ Notes.java
@@ -0,0 +1,51 @@
+Back-End Programming (2024)
+
+//Terminology:
+
+
+//Lecture 1:
+"Frond-End vs Back-End"
+-Front-End jobs are web designer and user experience designer
+-Back-End (server side) defines how the site works like updates, content management etc.
+
+"MVC design pattern
+-MVC (Model View Controller)
+-Is used for web application development
+-The roles
+	"*Model
+	maintaining the application data
+	"*View
+	-viewing data to end users
+	"*Controller
+	-user inputs and interact with model
+
+User >"uses"> Controller >"manipulates"> Model >"Updates"> View >"Sees"> User
+
+"Other Frameworks for Back-End
+Big Companies uses "Spring Boot"
+
+-Ruby on Rails
+-Django
+-ASP.NET
+-Express(Node.js)
+-Spring MVC(Java)
+etc.
+
+"Framework
+-Takes over the control of files in wants security(logins) 
+-Also Rest (library)
+-Also DB
+-Also Tests
+-For avoiding mistakes
+
+
+"Library
+-It have functions that you can call
+-For example C++ (stdio.h)
+
+//Q for V
+-CRUD
+-Frameworks
+-jar/war(one file etc.) standalone
+-initializing in VSCODE
+
